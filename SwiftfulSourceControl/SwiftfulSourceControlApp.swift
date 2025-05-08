aff5170d6670e9fb97eb49abcbc11c8cc3acb1db
Pavlo <pavlo.nord@gmail.com>
2025-05-08 12:49:11 +0300

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Pavlo Prokhorov on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulSourceControlTests/SwiftfulSourceControlTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftfulSourceControlTests.swift
+//  SwiftfulSourceControlTests
+//
+//  Created by Pavlo Prokhorov on 08.05.2025.
+//
+
+import Testing
+
+struct SwiftfulSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
