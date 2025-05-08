5689ed8ab048e3ce09558ae558f4199ef7aae347
Pavlo <pavlo.nord@gmail.com>
2025-05-08 21:43:22 +0300

    My second commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Image(systemName: "macbook")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "person.circle.fill")
+                    .padding(.horizontal)
+            }
+            .buttonStyle(.bordered)
+
         }
         .padding()
     }
