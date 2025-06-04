c2794ec1299868bfaa4a55574a8a2c471f269dab
Pavlo <pavlo.nord@gmail.com>
2025-06-04 10:07:33 +0300

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Pavlo Prokhorov on 04.06.2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Pavlo")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
